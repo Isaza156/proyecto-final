de0d6a108460cad191576a62757a26dde0c0d46c
John Doe <brahianestiven08@gmail.com>
2019-08-18 17:52:50 -0500

    Beta1

--- src/components/AccountComponent.jsx
+++ src/components/AccountComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Perfil from '../images/PerfilAccount.png';
+import Perfil from '../images/avatar2.png';
 import Navbar from './NavbarComponent';
 
 class Account extends React.Component {
@@ -9,56 +9,68 @@ class Account extends React.Component {
         return (
             <React.Fragment>
                 <Navbar/>
-
-                <div className='row'>
-                    <div className='col-md-6 col-sm-12 text-center'>
-                        <img src={Perfil} className="img-fluid my-4" alt="Perfil" />
-                        <h5>Patricia Cano</h5>
-                        <small> patricia@gmail.com </small>
+                <div>
+                <div className='row col-12 m-auto' id='confirmar'>
+                    <div className='col-md-6 col-sm-12 col-xs-12 text-center m-auto'>
+                        <div className='row col-12 m-auto'>
+                            <div className='col-md-6 col-sm-12 col-xs-12 text-center m-auto'>
+                                <img src={Perfil} className="img-fluid m-auto" alt="Perfil" />
+                            </div>
+                            <div className='m-auto'>
+                                <h1 className="mb-3 font-weight-bold">Patricia Cano</h1>
+                                <h5 className="text-dark" >patriciacano@gmail.com</h5>
+                                
+                            </div>
+                        </div>
                     </div>
-                    <div className='col-md-6 col-sm-12 d-flex flex-column align-items-center'>
-                    <Link to='/driver'>
-                        <button type="button" className="btn btn-primary px-5 mt-5 mb-4">Ser Conductor</button>
-                        </Link>
-                        <button type="button" className="btn btn-danger px-5 mb-3">Compartir</button>
+
+                    <div className='col-md-6 col-sm-12 col-xs-12 text-center m-auto'>
+
+                        <div className="row">
+                            <div className='m-auto'>
+                            <Link to='/driver'>
+                                <button type="button" className="btn btn-primary w-100 mb-3">Ser conductor</button>
+                            </Link>
+                                <button type="button" className="btn btn-danger w-100">Compartir</button>
+                            </div>
+                        </div>
+
+                       
                     </div>
                 </div>
 
                 <hr />
 
-                <div className='text-center mb-5'>
-                    <h3 className='my-5'>Historial de Viajes</h3>
-                    <div className='row justify-content-center'>
-                        <div className='border border-dark text-center p-2 m-2 bg-danger text-white'>
-                            <h5>Jose Perez</h5>
-                            <small>Envigado - Poblado </small>
+                <div className='text-center mb-5 '>
+                    <h3 className='mt-4 mb-4 font-weight-bold'>Historial de viajes</h3>
+                    <div className='row col-12 justify-content-center m-auto'>
+                        <div className='container__viajeros color__secondary col-md-3 col-sm-12'>
+                            <h4>Jose Perez</h4>
+                            <small>De Envigado hasta Poblado </small>
                             <br />
-                            <small>23 Ago 2019</small>
+                            <small>23 Ago 2019 -7:00a.m</small>
                         </div>
-                        <div className='border border-dark text-center p-2 m-2 bg-danger text-white'>
-                            <h5>Jose Perez</h5>
-                            <small>Envigado - Poblado </small>
+                        <div className='container__viajeros color__secondary col-md-3 col-sm-12'>
+                            <h4>Jose Perez</h4>
+                            <small>De Envigado hasta Ayura </small>
                             <br />
-                            <small>24 Ago 2019</small>
+                            <small>24 Ago 2019 -7:10a.m</small>
                         </div>
-                        <div className='border border-dark text-center p-2 m-2 bg-danger text-white'>
-                            <h5>Jose Perez</h5>
-                            <small>Envigado - Poblado </small>
+                        <div className='container__viajeros color__secondary col-md-3 col-sm-12'>
+                            <h4>Jose Perez</h4>
+                            <small>De Envigado hasta Poblado </small>
                             <br />
-                            <small>25 Ago 2019</small>
+                            <small>25 Ago 2019 -7:20a.m</small>
                         </div>
-                        <div className='border border-dark text-center p-2 m-2 bg-danger text-white'>
-                            <h5>Jose Perez</h5>
-                            <small>Envigado - Poblado </small>
-                            <br />
-                            <small>26 Ago 2019</small>
-                        </div>
+
                     </div>
                 </div>
-
+                </div>
+               
             </React.Fragment>
         );
     }
 }
 
 export default Account;
+
--- src/components/DriverComponent.jsx
+++ src/components/DriverComponent.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Perfil from '../images/PerfilAccount.png';
+import Perfil from '../images/avatar2.png';
 import Navbar from './NavbarComponent';
+
+import '../styles/DriverStyle.css';
 
 class Driver extends React.Component {
     state = {}
@@ -9,44 +11,86 @@ class Driver extends React.Component {
         return (
             <React.Fragment>
                 <Navbar />
-                <div className='row' id='confirmar'>
-                    <div className='col-md-6 col-sm-12 text-center'>
+                <div>
+                <div className='row col-12 m-auto'>
+                <div className='col-md-6 col-sm-12 col-xs-12 text-center m-auto'>
                         <div className='row'>
-                            <div className='col-md-6 col-sm-12 text-center'>
-                                <img src={Perfil} className="img-fluid mt-4 mb-4" alt="Perfil" />
-                                <h5>Jose Perez</h5>
+                            <div className='col-md-6 col-sm-12 col-xs-12 text-center m-auto'>
+                                <img src={Perfil} className="img-fluid mx-auto w-50" alt="Perfil" />
+                                <h3>Patricia Cano</h3>
+                                <h5>patriciacano@gmail.com</h5>
+
                             </div>
-                            <div className='my-4 col-md-6 col-sm-12'>
-                                <input type='text' className="text-dark text-center bg-light my-2" placeholder='Placa: AAA-000'/>
-                                <input type='text' className="text-dark text-center bg-light my-2" placeholder='Color: Rojo'/>
-                                <input type='text' className="text-dark text-center bg-light my-2" placeholder='Salida: Envigado'/>
-                                <input type='text' className="text-dark text-center bg-light my-2" placeholder='Precio: $0'/>
+                            <div className='row m-auto text-left flex-column'>
+                                <input type='text' className="text-dark text-center bg-light  border border-info rounded py-1 m-2" placeholder='Placa: AAA-000'/>
+                                <input type='text' className="text-dark text-center bg-light  border border-info rounded py-1 m-2" placeholder='Color: Rojo'/>
+                                <input type='text' className="text-dark text-center bg-light  border border-info rounded py-1 m-2" placeholder='Salida: Envigado'/>
+                                <input type='text' className="text-dark text-center bg-light  border border-info rounded py-1 m-2" placeholder='Precio: $0'/>
                             </div>
                         </div>
                     </div>
-                    <div className='col-md-6 col-sm-12 d-flex flex-column align-items-center'>
-                    <Link to='/principal'>
-                        <button type="button" className="btn btn-primary px-5 mt-5 mb-4">Ser Pasajero</button>
-                        </Link>
-                        <Link to='/service'>
-                        <button type="button" className="btn btn-danger px-5 mb-4">Guardar Cambios</button>     
-                        </Link>
-                                     
+
+                    <div className='col-md-6 col-sm-12 col-xs-12 text-center m-auto'>
+                    <div className='row'>
+                        <div className='m-auto'>
+                            <Link to='/principal'>
+                                <button type="button" className="btn btn-primary w-100 mb-3">Ser usuario</button>
+                            </Link>
+                                <button type="button" className="btn btn-danger w-100">Guardar cambios</button>
+                        </div>
+
                     </div>
-                    
+                        
+                    </div>
                 </div>
 
                 <hr />
+                <div className='text-center mb-5 '>
+                    <h3 className='mt-4 mb-4 font-weight-bold'>Personas en espera</h3>
+                    <div className='row col-12 justify-content-center m-auto'>
+                        <div className='container__viajeros color__secondary col-md-4 col-sm-12'>
+                            <h4>Manuel Andres</h4>
+                            <small>De Envigado hasta Poblado </small>
+                            <br />
+                            <small>23 Ago 2019 -7:00a.m</small>
+                            <button className="btn btn-primary m-2">aceptar</button>
+                        </div>
+                        <div className='container__viajeros color__secondary col-md-4 col-sm-12'>
+                            <h4>Sofia Hernandez</h4>
+                            <small>De Envigado hasta Ayura </small>
+                            <br />
+                            <small>23 Ago 2019 -7:10a.m</small>
+                            <button className="btn btn-primary m-2">aceptar</button>
+                        </div>
+                        <div className='container__viajeros color__secondary col-md-4 col-sm-12'>
+                            <h4>Yeferson Perea</h4>
+                            <small>De Envigado hasta Poblado </small>
+                            <br />
+                            <small>23 Ago 2019 -7:20a.m</small>
+                            <button className="btn btn-primary m-2">aceptar</button>
 
-                <div className='text-center mb-5'>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3966.6827442126423!2d-75.61288418590692!3d6.17321372885054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sco!4v1565896568458!5m2!1ses!2sco" width="1200" height="200" frameBorder="0" allowFullScreen></iframe>
+                        </div>
+                        <div className='container__viajeros color__secondary col-md-4 col-sm-12'>
+                            <h4>Yeferson Perea</h4>
+                            <small>De Envigado hasta Poblado </small>
+                            <br />
+                            <small>23 Ago 2019 -7:20a.m</small>
+                            <button className="btn btn-primary m-2">aceptar</button>
+
+                        </div>
+
+                    </div>
+                    <Link to="/router">
+                    <button className="btn btn-primary m-2 w-50">Comenzar viaje</button>
+                    </Link>
                 </div>
 
-                
-                
-
+                        
+               </div>
+                    
             </React.Fragment>
         );
     }
 }
 export default Driver;
+
